5d0062a35ee64894525acfe90d22e3c0ea003a0b
Amadeus Her <Amadeusherr@gmail.com>
2020-12-10 12:35:33 -0600

    Add the ability to login

--- client/src/components/login/Login.js
+++ client/src/components/login/Login.js
@@ -1,7 +1,13 @@
 import { Backdrop, Button,  Fade,  makeStyles,  Modal,  TextField, Typography } from '@material-ui/core';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import '../../styles/login.css';
 import Register from './Register';
+import propTypes from 'prop-types';
+import {connect} from 'react-redux';
+import { login } from '../../flux/actions/auth';
+import { clearErrors } from '../../flux/actions/error';
+import { Alert } from '@material-ui/lab';
+
 
 const useStyles = makeStyles((theme) => ({
     modal: {
@@ -16,10 +22,14 @@ const useStyles = makeStyles((theme) => ({
     },
   }));
 
-function Login(){
+function Login(props){
 
     const classes = useStyles();
     const [open, setOpen] = useState(false);
+    const[msg, setMsg] = useState(null);
+    const[info, setInfo] = useState({email:'', password:''});
+    const[submitted, setSubmitted] = useState(false);
+    const{error, isAuthenticated} = props
 
     const handleOpen = () => {
         setOpen(true);
@@ -30,34 +40,63 @@ function Login(){
     };
     const loginSubmit = (event) => {
         event.preventDefault();
-        console.log("hello");
+        props.login(info);
+        setSubmitted(true);
+        event.target.reset();
     }
+
+    const infoChange = (event) => {
+        setInfo({...info, [event.target.name]: event.target.value})
+    }
+
+    useEffect(() => {
+        if (error.id === 'LOGIN_FAIL') {
+            setMsg(error.msg.msg);
+        } else {
+            setMsg(null);
+        }
+    }, [error])
+    
+
     return(
         <div className="form-container">
             <form className="input-form" onSubmit={loginSubmit}>
                 <Typography style={{textAlign: "center", fontWeight: "bold", fontSize: "25px"}}>LOGIN FORM</Typography>
-                <TextField fullWidth label="Email" variant="outlined" className="input-login" placeholder="example@gmail.com"  />
-                <TextField fullWidth label="Password" variant="outlined" className="input-login" placeholder="password" />
+                <TextField fullWidth name="email" label="Email" variant="outlined" className="input-login" placeholder="example@gmail.com" onChange={infoChange} />
+                <TextField fullWidth name="password" label="Password" variant="outlined" className="input-login" placeholder="password" onChange={infoChange} />
                 <Button type="submit" className="form-button" variant="outlined">Login</Button>
                 <Button style={{float: "right"}} className="form-button" variant="outlined" onClick={handleOpen}>Register</Button>
+                {msg !== null && submitted? <Alert className="input-login" color="error">{msg}</Alert> : null}
             </form>
-            <Modal
-                aria-labelledby="transition-modal-title"
-                aria-describedby="transition-modal-description"
-                className={classes.modal}
-                open={open}
-                onClose={handleClose}
-                closeAfterTransition
-                BackdropComponent={Backdrop}
-            >
-                <Fade in={open}>
-                    <div className={classes.paper}>
-                        <Register />
-                    </div>
-                </Fade>
-            </Modal>
+                <Modal
+                    aria-labelledby="transition-modal-title"
+                    aria-describedby="transition-modal-description"
+                    className={classes.modal}
+                    open={open}
+                    onClose={handleClose}
+                    closeAfterTransition
+                    BackdropComponent={Backdrop}
+                >
+                    <Fade in={open}>
+                        <div className={classes.paper}>
+                            <Register />
+                        </div>
+                    </Fade>
+                </Modal>
             </div>    
     )
 }
 
-export default Login;
+Login.propTypes = {
+    isAuthenticated: propTypes.bool,
+    login: propTypes.func.isRequired,
+    clearErrors: propTypes.func.isRequired
+}
+
+const mapStateToProps = state => ({
+    isAuthenticated: state.auth.isAuthenticated,
+    error: state.error
+})
+
+
+export default connect(mapStateToProps, {login, clearErrors})(Login);
