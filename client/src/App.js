2c4610ac58f41a5089c0b326de2a478f1b7dc137
Amadeus Her <Amadeusherr@gmail.com>
2020-12-10 12:34:24 -0600

    Add store provider

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,20 @@
+import { useEffect } from 'react';
+import { Provider } from 'react-redux';
 import './App.css';
 import Navbar from './components/header/Navbar';
+import { loadUser } from './flux/actions/auth';
+import store from './flux/store.js';
 
 function App() {
+  useEffect(() => {
+    store.dispatch(loadUser())
+  }, [])
   return (
-    <div className="main-container">
-      <Navbar />
-    </div>
+    <Provider store={store}>
+      <div className="main-container">
+        <Navbar />
+      </div>
+    </Provider>
   );
 }
 
