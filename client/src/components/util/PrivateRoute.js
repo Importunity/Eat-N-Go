4b9a00762d2e5fccd71af4d0d8934b6466a3c412
Amadeus Her <Amadeusherr@gmail.com>
2020-12-10 12:42:55 -0600

    Add utilities component

---
+++ client/src/components/util/PrivateRoute.js
@@ -0,0 +1,14 @@
+import { Redirect, Route } from "react-router-dom"
+
+
+export const PrivateRoute  = ({component: Component, authed, ...rest}) => {
+    return (
+      <Route
+        {...rest}
+        render={(props) => authed === true
+          ? <Component {...props} />
+          : <Redirect to={{pathname: '/login', state: {from: props.location}}} />}
+      />
+    )
+  }
+  
